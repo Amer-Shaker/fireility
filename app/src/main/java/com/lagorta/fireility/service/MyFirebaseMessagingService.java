d50bccc09e1edddeee8348bba2c6354d37beeb4e
Amer Shaker <a.shaker@Lagorta.local>
2020-01-06 16:34:06 +0200

    d

--- app/src/main/java/com/lagorta/fireility/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/lagorta/fireility/service/MyFirebaseMessagingService.java
@@ -1,6 +1,7 @@
 package com.lagorta.fireility.service;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
@@ -11,6 +12,7 @@ import androidx.work.WorkManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.lagorta.fireility.view.MainActivity;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -45,6 +47,9 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
 
             Handler handler = new Handler(Looper.getMainLooper());
             handler.post(new Runnable() {
--- app/src/main/java/com/lagorta/fireility/view/MainActivity.java
+++ app/src/main/java/com/lagorta/fireility/view/MainActivity.java
@@ -1,43 +1,39 @@
-//package com.lagorta.fireility.view;
-//
-//import android.content.BroadcastReceiver;
-//import android.content.Context;
-//import android.content.Intent;
-//import android.content.IntentFilter;
-//import android.os.Bundle;
-//import android.util.Log;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
-////
-////    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
-////        @Override
-////        public void onReceive(Context context, Intent intent) {
-////            Log.d("Broadcast", "Subscribing to test-library topic");
-////
-////            // Extract data included in the Intent
-////            String t = intent.getStringExtra("value1");
-////            String t1 = intent.getStringExtra("value2");
-////            //alert data here
-////        }
-////    };
-////
-////    @Override
-////    public void onResume() {
-////        super.onResume();
-////        LocalBroadcastManager.getInstance(MainActivity.this).registerReceiver(mMessageReceiver, new IntentFilter("myFunction"));
-////    }
-////
-////    @Override
-////    public void onPause() {
-////        super.onPause();
-////        LocalBroadcastManager.getInstance(MainActivity.this).unregisterReceiver(mMessageReceiver);
-////    }
-//}
+package com.lagorta.fireility.view;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Write your message here.");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+}
