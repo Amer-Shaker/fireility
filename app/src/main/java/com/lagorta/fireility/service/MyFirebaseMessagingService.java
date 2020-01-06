f51bb8c20a2aa5d85ba2c19d688d55f3a95850fd
Amer Shaker <a.shaker@Lagorta.local>
2020-01-06 11:40:12 +0200

    f

--- app/src/main/java/com/lagorta/fireility/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/lagorta/fireility/service/MyFirebaseMessagingService.java
@@ -47,8 +47,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            if (/* Check if data needs to be processed by long running job */ false) {
+            handleNow();
+            if (/* Check if data needs to be processed by long running job */ true) {
                 // For long-running tasks (10 seconds or more) use WorkManager.
                 scheduleJob();
             } else {
@@ -58,7 +58,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
 
         // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
+        if (remoteMessage.getNotification() != null && remoteMessage.getData().isEmpty()) {
 
             Handler handler = new Handler(Looper.getMainLooper());
             handler.post(new Runnable() {
