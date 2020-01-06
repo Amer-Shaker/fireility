da5519f0a0c4ef5538fdf45167ff606dad8d6bc1
Amer Shaker <a.shaker@Lagorta.local>
2020-01-06 14:44:50 +0200

    d

--- app/src/main/java/com/lagorta/fireility/view/MainActivity.java
+++ app/src/main/java/com/lagorta/fireility/view/MainActivity.java
@@ -1,43 +1,43 @@
-package com.lagorta.fireility.view;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Bundle;
-import android.util.Log;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.d("Broadcast", "Subscribing to test-library topic");
-
-            // Extract data included in the Intent
-            String t = intent.getStringExtra("value1");
-            String t1 = intent.getStringExtra("value2");
-            //alert data here
-        }
-    };
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        LocalBroadcastManager.getInstance(MainActivity.this).registerReceiver(mMessageReceiver, new IntentFilter("myFunction"));
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        LocalBroadcastManager.getInstance(MainActivity.this).unregisterReceiver(mMessageReceiver);
-    }
-}
+//package com.lagorta.fireility.view;
+//
+//import android.content.BroadcastReceiver;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.content.IntentFilter;
+//import android.os.Bundle;
+//import android.util.Log;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+////
+////    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+////        @Override
+////        public void onReceive(Context context, Intent intent) {
+////            Log.d("Broadcast", "Subscribing to test-library topic");
+////
+////            // Extract data included in the Intent
+////            String t = intent.getStringExtra("value1");
+////            String t1 = intent.getStringExtra("value2");
+////            //alert data here
+////        }
+////    };
+////
+////    @Override
+////    public void onResume() {
+////        super.onResume();
+////        LocalBroadcastManager.getInstance(MainActivity.this).registerReceiver(mMessageReceiver, new IntentFilter("myFunction"));
+////    }
+////
+////    @Override
+////    public void onPause() {
+////        super.onPause();
+////        LocalBroadcastManager.getInstance(MainActivity.this).unregisterReceiver(mMessageReceiver);
+////    }
+//}
