a3a6fc392eaaebe9cb7c70dbfc43ee05f1a07017
Amer Shaker <a.shaker@Lagorta.local>
2020-01-06 14:03:54 +0200

    d

--- app/src/main/java/com/lagorta/fireility/service/MyWorker.java
+++ app/src/main/java/com/lagorta/fireility/service/MyWorker.java
@@ -1,11 +1,15 @@
 package com.lagorta.fireility.service;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+
+import com.lagorta.fireility.view.MainActivity;
 
 public class MyWorker extends Worker {
 
@@ -13,6 +17,23 @@ public class MyWorker extends Worker {
 
     public MyWorker(@NonNull Context appContext, @NonNull WorkerParameters workerParams) {
         super(appContext, workerParams);
+
+        new AlertDialog.Builder(appContext)
+                .setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this entry?")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 
     @NonNull
